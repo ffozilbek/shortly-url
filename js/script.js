40d64f28dd074919f188a311edadc383c8edcecd
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-06-17 21:45:21 +0500

    add statistiks

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@ const elShorterLinks = document.querySelector(".url-shortener__shorter-links"),
     elUrlShortenerForm = document.querySelector(".js-url-shortener-form"),
     elNewShortLinkCopierBtns = document.querySelectorAll(".js-url-shortener__shorter-link-button");
 
-    elUrlShortenerForm .addEventListener("submit", (e)=> {
-        e.preventDefault();
-        elShorterLinks.classList.toggle("url-shortener__shorter-links--open");
-    })
+    if(elUrlShortenerForm) {
+        elUrlShortenerForm .addEventListener("submit", (e)=> {
+            e.preventDefault();
+            elShorterLinks.classList.toggle("url-shortener__shorter-links--open");
+        })
+    }
 
     elNewShortLinkCopierBtns.forEach(elNewShortLinkCopierBtn => {
         elNewShortLinkCopierBtn.addEventListener("click", ()=> {
-            if(elNewShortLinkCopierBtn.contains('button--clicked')) {
-                elNewShortLinkCopierBtn.classList.add("button--clicked")
-            }
+            elNewShortLinkCopierBtn.textContent="Copied!";
+            elNewShortLinkCopierBtn.classList.add("button--clicked");
+            setTimeout(() => {
+                elNewShortLinkCopierBtn.textContent="Copy";
+            elNewShortLinkCopierBtn.classList.remove("button--clicked");
+            }, 3000);
         })
     });
