b428d4db6852b9abdbe4e1215ef9d9052ae80798
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-10-29 22:10:04 +0500

    make responsive navbar

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
+const elBurger = document.querySelector(".burger"),
+    elHeaderNav = document.querySelector(".header__navbar");
+
+if(elBurger) {
+    elBurger.addEventListener("click", ()=> {
+        elHeaderNav.classList.toggle("active")
+    })
+}
+
+
+
+
 const elShorterLinks = document.querySelector(".url-shortener__shorter-links"),
-    elUrlShortenerForm = document.querySelector(".js-url-shortener-form"),
-    elNewShortLinkCopierBtns = document.querySelectorAll(".js-url-shortener__shorter-link-button");
+  elUrlShortenerForm = document.querySelector(".js-url-shortener-form"),
+  elNewShortLinkCopierBtns = document.querySelectorAll(
+    ".js-url-shortener__shorter-link-button"
+  );
 
-    if(elUrlShortenerForm) {
-        elUrlShortenerForm .addEventListener("submit", (e)=> {
-            e.preventDefault();
-            elShorterLinks.classList.toggle("url-shortener__shorter-links--open");
-        })
-    }
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    elShorterLinks.classList.toggle("url-shortener__shorter-links--open");
+  });
+}
 
-    elNewShortLinkCopierBtns.forEach(elNewShortLinkCopierBtn => {
-        elNewShortLinkCopierBtn.addEventListener("click", ()=> {
-            elNewShortLinkCopierBtn.textContent="Copied!";
-            elNewShortLinkCopierBtn.classList.add("button--clicked");
-            navigator.clipboard.writeText(elNewShortLinkCopierBtn.previousElementSibling.textContent);
-            setTimeout(() => {
-                elNewShortLinkCopierBtn.textContent="Copy";
-                elNewShortLinkCopierBtn.classList.remove("button--clicked");
-            }, 400);
-        })
-    });
+elNewShortLinkCopierBtns.forEach((elNewShortLinkCopierBtn) => {
+  elNewShortLinkCopierBtn.addEventListener("click", () => {
+    elNewShortLinkCopierBtn.textContent = "Copied!";
+    elNewShortLinkCopierBtn.classList.add("button--clicked");
+    navigator.clipboard.writeText(
+      elNewShortLinkCopierBtn.previousElementSibling.textContent
+    );
+    setTimeout(() => {
+      elNewShortLinkCopierBtn.textContent = "Copy";
+      elNewShortLinkCopierBtn.classList.remove("button--clicked");
+    }, 400);
+  });
+});
